9b28e145ed363933c16a60c9ca158cc9bb456e63
Jacob Hein <jacobhein1892@gmail.com>
2023-08-01 15:54:08 -0700

    set up outline for my front end

--- front/src/pages/App.js
+++ front/src/pages/App.js
@@ -1,5 +1,16 @@
 import '../styles/App.sass';
 
+/*
+ * This is gonig to be a single page app to start with, and will essentially be just an  
+ * MVP to see if this idea works.
+ * 
+ * I will use multiple components:
+ *    First is an initial video input (Will turn into a carosel for multiple videos to    
+ *    choose from)
+ *    Second will be an input video option (With option later to use Webcam)
+ *    Thrid is a comparison score output generator
+ * */ 
+
 function App() {
   return (
     <div className="App">
