f5d0daa77b95ef8ccbe750323bc6271c13ce957e
Jacob Hein <jacobhein1892@gmail.com>
2023-08-01 17:13:28 -0700

    set up the general outline for how I want this page to be shown

--- front/src/components/main.js
+++ front/src/components/main.js
@@ -1,11 +1,20 @@
 import React from "react"
 import "../styles/Main.sass"
+import VideoSection from "./videoSection"
 
 export default function MainPage()
 {
 
     return (
         <div className="main_wrapper">
+            <div className="input_wrapper">    
+                <VideoSection/>
+                <VideoSection/>
+            </div>
+            <div>
+                <h1>Comparison Score Section</h1>
+            </div>
+
         </div>
     )
 }
---
+++ front/src/components/videoSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../styles/Video.sass"
+
+export default function VideoSection()
+{
+
+
+    return (
+        <div className="video_wrapper">
+            <h1>Video Section</h1>
+        </div>
+    )
+}
