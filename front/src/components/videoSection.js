93d3da2033c7bed61a031508635943d067ede4be
Jacob Hein <jacobhein1892@gmail.com>
2023-08-03 09:30:08 -0700

    able to upload videos to the front end. Going to fix a little bit of styling

--- front/src/components/videoSection.js
+++ front/src/components/videoSection.js
@@ -3,15 +3,31 @@ import "../styles/Video.sass"
 
 export default function VideoSection({type})
 {
+    const [selectedVideo, setSelectedVideo] = useState(null);
+    /**
+     * 1) Upload Video
+     * 2) Pass Video to backend
+     * 3) Get Video back from backend
+     * 4) Display the video in video section
+     */
 
+    const handleVideoChange = (event) => {
+        const file = event.target.files[0];
+        setSelectedVideo(file);
+    }
 
     return (
         <div className="video_wrapper">
             <div className="top_section">
                 <h1>Video Section: {type}</h1>
-                <input type="file" />
+                <input type="file" accept='video/' onChange={handleVideoChange} />
             </div>
             <div className="video_section">
+                {selectedVideo && 
+                    <video controls width="400">
+                        <source src={URL.createObjectURL(selectedVideo)} type="video/mp4" />
+                    </video>
+                }
             </div>
         </div>
     )
