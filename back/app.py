b70bb72358b8d035868f89d645348cf8b360d69e
Jacob Hein <jacobhein1892@gmail.com>
2023-08-04 12:30:50 -0700

    I think I have the framework for the backend

--- back/app.py
+++ back/app.py
@@ -5,6 +5,15 @@ import cv2
 from flask_cors import CORS
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER  = 'uploads'
+ANNOTATED_FOLDER = 'annotated'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['ANNOTATED_FOLDER'] = ANNOTATED_FOLDER
+
+
+
+
 CORS(app, origins=["http://localhost:3000"], methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
 
 @app.route('/')
@@ -22,7 +31,47 @@ def uploadVideo():
 
 @app.route('/process_video', methods=['POST'])
 def process_video():
+    if 'video' in request.files:
+        video_file = request.files['video']
+        if video_file.filename != '':
+            filename = secure_filename(video_file.filename)
+            video_filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            video_file.save(video_filepath)
+
+            annotated_filepath = os.path.join(app.config['ANNOTATED_FOLDER'], filename)
+            process_and_annotate_video(video_filepath, annotated_filepath)
+
+            annotate_url = f'http://localhost:5000/annotated/{filename}'
+            return jsonify({'message': 'Video processed and annotated', 'annotatedUrl': annotate_url }), 200
+        
+    return jsonify({'error': 'No video file in request'}), 400
+
+
+def process_and_annotate_video(input_filepath, output_filepath):
+    cap = cv2.VideoCapture(input_filepath)
+     
+     ## OpenCV pose recognition and annotation code goes here
+
+    out = cv2.VideoWriter(output_filepath, cv2.VideoWriter_fourcc(*'mp4v'), cap.get(cv2.CAP_PROP_FPS), (int(cap.get(3)), int(cap.get(4))))
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        ## Perform pose recignition and annotation on the frame
+
+        out.write(frame)
     
+    cap.release()
+    out.release()
+
+@app.route('/annotated/<filename>')
+def get_annotated_video(filename):
+    return send_from_directory(app.config['ANNOTATED_FOLDER'], filename)
+    
+
+
 
 if __name__ == "__main__":
     app.run()
