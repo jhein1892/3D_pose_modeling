28c77e38f63debb64d1220097c9ab06661085f80
Jacob Hein <jacobhein1892@gmail.com>
2023-09-08 11:14:52 -0700

    displaying all scores for each LM on home page

--- front/src/components/main.js
+++ front/src/components/main.js
@@ -5,10 +5,29 @@ import VideoSection from "./videoSection"
 
 export default function MainPage()
 {
-
     const [videoTitle, setVideoTitle] = useState({"User":null, "Coach": null})
     const [compAccuracy, setCompAccuracy] = useState();
     const [lmAccuracy, setLmAccuracy] = useState();
+
+    const lmNames = {
+        0: "Nose",
+        2: "Left Eye",
+        5: "Right Eye",
+        7: "Left Ear",
+        8: "Right Ear",
+        11: "Left Shoulder", 
+        12: "Right Shoulder",
+        13: "Left Elbow",
+        14: "Right Elbow",
+        15: "Left Wrist",
+        16: "Right Wrist",
+        23: "Left Hip",
+        24: "Right Hip",
+        25: "Left Knee",
+        26: "Right Knee",
+        27: "Left Ankle",
+        28: "Right Ankle"
+    }
 
     function handleVideoCompare(){
         // Need to pass a video to the backend
@@ -28,17 +47,44 @@ export default function MainPage()
         })
     }
 
+    function generateScores(scores){
+        return scores.map((score) => {
+            let lmScore = (lmAccuracy[score] * 100).toFixed(2)
+            return (
+                <div>
+                    <h4>{lmNames[score]}: %{lmScore}</h4>
+                </div>
+            )
+        })
+    }
+
     return (
         <div className="main_wrapper">
             <div className="input_wrapper">    
                 <VideoSection userType={"User"} setVideoTitle={setVideoTitle}/>
-                <button onClick={handleVideoCompare}>Compare Vids</button>
+                <div>
+                    {compAccuracy&&
+                        <h3>%{compAccuracy}</h3>
+                    }
+                    <button onClick={handleVideoCompare}>Compare Vids</button>
+                </div>
                 <VideoSection userType={"Coach"} setVideoTitle={setVideoTitle}/>
             </div>
             <div className="comparison_wrapper">
                 <h1>Comparison Score Section</h1>
-                <div>
-                    <h3>Total Score: %{compAccuracy&& <span>{compAccuracy}</span>}</h3>
+                <div className="parts_wrapper">
+                    <div>
+                        <h3>Head</h3>
+                        {generateScores([0,2,5,7,8])}
+                    </div>
+                    <div>
+                        <h3>Upper Body</h3>
+                        {generateScores([11,12,13,14,15,16])}
+                    </div>
+                    <div>
+                        <h3>Lower Body</h3>
+                        {generateScores([23,24,25,26,27,28])}
+                    </div>
                 </div>
             </div>
 
