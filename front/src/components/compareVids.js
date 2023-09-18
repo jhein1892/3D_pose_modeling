4f76c27a7de71e53efb00c3d87e69f0e518645b5
Jacob Hein <jacobhein1892@gmail.com>
2023-09-18 09:56:23 -0700

    seperated my comparison module into different file

---
+++ front/src/components/compareVids.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import classNames from 'classnames'
+import "../styles/CompVids.sass"
+
+export default function CompareVids({compAccuracy, dashArray})
+{
+    const circleClass = classNames({
+        "circle": compAccuracy != null,
+        "circle_NA": compAccuracy == null
+    })
+
+    const circleColors = classNames('circular-chart', {
+        "red": compAccuracy && compAccuracy <= 50,
+        "orange": compAccuracy && compAccuracy <= 60 && compAccuracy > 50,
+        "yellow": compAccuracy && compAccuracy <= 70 && compAccuracy > 60,
+        "greenYellow":compAccuracy && compAccuracy <= 80 && compAccuracy > 70,
+        "green": compAccuracy && compAccuracy > 80
+    })
+
+    return (
+        <div class="flex-wrapper">
+            <div class="single-chart">
+                <svg viewBox="0 0 36 36" className={circleColors}>
+                <path class="circle-bg"
+                    d="M18 2.0845
+                    a 15.9155 15.9155 0 0 1 0 31.831
+                    a 15.9155 15.9155 0 0 1 0 -31.831"
+                />
+                <path className={circleClass}
+                    strokeDasharray={dashArray}
+                    d="M18 2.0845
+                    a 15.9155 15.9155 0 0 1 0 31.831
+                    a 15.9155 15.9155 0 0 1 0 -31.831"
+                />
+                {compAccuracy ?
+                    <text x="18" y="20.35" class="percentage">{compAccuracy}%</text>
+                    :
+                    <text x="18" y="20.35" class="percentage">NA</text>
+                }
+                </svg>
+            </div>
+        </div>
+    )
+}
--- front/src/components/main.js
+++ front/src/components/main.js
@@ -2,6 +2,7 @@ import React, {useState} from "react"
 import axios from "axios"
 import "../styles/Main.sass"
 import VideoSection from "./videoSection"
+import CompareVids from "./compareVids"
 import classNames from 'classnames'
 
 export default function MainPage()
@@ -36,16 +37,12 @@ export default function MainPage()
         const formData = new FormData();
         formData.append('User', videoTitle['User'])
         formData.append('Coach', videoTitle['Coach'])
-        
-        // setVideoTitle(prev => ({...prev, User: formData}))
+    
         axios.post("http://127.0.0.1:5000/compare_videos", formData, {
             headers: {'Content-Type': 'multipart/form-data'}
         })
         .then((response) => {
-            console.log(response.data)
-            // let totAccuracy = (response.data.accuracy * 100).toFixed(2)
             let totAccuracy = Math.round(response.data.accuracy * 100)
-            // let totAccuracy = response.data.accuracy
             setDashArray(`${totAccuracy}, 100`)
             setCompAccuracy(totAccuracy)
             setLmAccuracy(response.data.normalized)
@@ -65,55 +62,17 @@ export default function MainPage()
                 <div className="parts_col">
                     <h4>{lmNames[score]}:</h4>
                     {lmAccuracy ? <span className={divClasses}>{(lmAccuracy[score] * 100).toFixed(2)}%</span>: <span>NA</span>}
-                    {/* {lmAccuracy ? <span className={divClasses}>{lmAccuracy[score]}%</span>: <span>NA</span>} */}
                 </div>
             )
         })
     }
-
-    const circleClass = classNames({
-        "circle": compAccuracy != null,
-        "circle_NA": compAccuracy == null
-    })
-
-    const circleColors = classNames('circular-chart', {
-        "red": compAccuracy && compAccuracy <= 50,
-        "orange": compAccuracy && compAccuracy <= 60 && compAccuracy > 50,
-        "yellow": compAccuracy && compAccuracy <= 70 && compAccuracy > 60,
-        "greenYellow":compAccuracy && compAccuracy <= 80 && compAccuracy > 70,
-        "green": compAccuracy && compAccuracy > 80
-    })
 
     return (
         <div className="main_wrapper">
             <div className="input_wrapper">    
                 <VideoSection userType={"User"} setVideoTitle={setVideoTitle}/>
                 <div className="comp_section">
-                <div class="flex-wrapper">
-                    <div class="single-chart">
-                        <svg viewBox="0 0 36 36" className={circleColors}>
-                        <path class="circle-bg"
-                            d="M18 2.0845
-                            a 15.9155 15.9155 0 0 1 0 31.831
-                            a 15.9155 15.9155 0 0 1 0 -31.831"
-                        />
-                        <path className={circleClass}
-                            strokeDasharray={dashArray}
-                            d="M18 2.0845
-                            a 15.9155 15.9155 0 0 1 0 31.831
-                            a 15.9155 15.9155 0 0 1 0 -31.831"
-                        />
-                        {compAccuracy ?
-                            <text x="18" y="20.35" class="percentage">{compAccuracy}%</text>
-                            :
-                            <text x="18" y="20.35" class="percentage">NA</text>
-                        }
-                        </svg>
-                    </div>
-                </div>
-                    {/* {compAccuracy&&
-                        <h3>%{compAccuracy}</h3>
-                    } */}
+                    <CompareVids compAccuracy={compAccuracy} dashArray={dashArray}/>
                     <button onClick={handleVideoCompare}>Compare Vids</button>
                 </div>
                 <VideoSection userType={"Coach"} setVideoTitle={setVideoTitle}/>
